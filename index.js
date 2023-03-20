9f17284067053cb0ed4fa9b8e5e71ca5f86c9783
aramedis <119070223+aramedis@users.noreply.github.com>
2023-03-20 09:32:12 +0000

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+import Manager from "./lib/Manager";
+import Engineer from "./lib/Engineer";
+import Intern from "./lib/Intern";
+import inquirer from "inquirer";
+import path from "path";
+import fs from "fs";
+
+const OUTPUT_DIR = path.resolve(__dirname, "output");
+const outputPath = path.join(OUTPUT_DIR, "team.html");
+
+import render from "./src/page-template.js";
+
+
+// TODO: Write Code to gather information about the development team members, and render the HTML file.
+
